b796897735d10a4d268b1ea6f6b85c51bfae3118
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-08-09 13:33:22 +0700

    feat/enable outh with github & google

--- auth.ts
+++ auth.ts
@@ -5,7 +5,24 @@ import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { 
+  handlers, 
+  signIn, 
+  signOut, 
+  auth 
+} = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() }
+      })
+    }
+  },
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
