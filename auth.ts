31d5c1732c4584edf631e2981fb88ee037837067
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-10-05 16:24:22 +0700

    feat/implement settings page

--- auth.ts
+++ auth.ts
@@ -5,12 +5,13 @@ import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+import { getAccountByUserId } from "./data/account";
 
 export const {
   handlers,
   signIn,
   signOut,
-  auth
+  auth,
 } = NextAuth({
   pages: {
     signIn: "/auth/login",
@@ -61,6 +62,12 @@ export const {
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
 
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
@@ -70,6 +77,11 @@ export const {
 
       if (!existingUser) return token;
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
